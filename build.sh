ff4f5d6ce2f5001022a38f4ec756d0a1f5c41d09
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-03-07 19:10:48 +0530

    added runtime dependencies

--- build.sh
+++ build.sh
@@ -3,17 +3,10 @@ echo installing Homebrew
 
 echo Downloading dependencies
 brew install cmake
-brew install nasm
-brew install node
-brew install ruby
-brew install pyenv
 brew install wget
 brew install zip
 brew install unzip
-brew install --cask dotnet
-brew install --cask dotnet-sdk
-pyenv install 3.10.2
-npm i headers
+brew install p7zip
 
 #!/bin/bash
 LOC= /Users/fareedahammad   # change path later to universal
@@ -25,16 +18,69 @@ cd metacall
 mkdir dependencies
 cd dependencies
 
-mkdir node_packages
-cd node_packages
+echo Installing Runtimes
+
+mkdir runtimes
+
+echo installing ruby
+mkdir runtimes/ruby
+cd runtimes/ruby
+wget https://github.com/MSP-Greg/ruby-loco/releases/download/ruby-master/ruby-mswin.7z    # --- i dont know which file is for mac
+7za e ruby-mswin.7z
+rm ruby-mswin.7z
+cd ruby-mswin
+./configure
+make
+cd ../
+
+echo installing python
+mkdir python
+cd python
+wget https://www.python.org/ftp/python/3.9.10/python-3.9.10-macos11.pkg
+installer -pkg python-3.9.10-macos11.pkg -target $LOC/Desktop/Metacall/metacall/dependencies/runtimes/python
+rm python-3.9.10-macos11.pkg
+cd ../
+
+echo installing dotnet
+mkdir dotnet
+cd dotnet
+# --- the below zip is for windows. so find dotnet installer for macos
+wget https://download.visualstudio.microsoft.com/download/pr/d1ca6dbf-d054-46ba-86d1-36eb2e455ba2/e950d4503116142d9c2129ed65084a15/dotnet-sdk-5.0.403-win-x64.zip  
+unzip dotnet-sdk-5.0.403-win-x64.zip
+rm dotnet-sdk-5.0.403-win-x64.zip
+cd dotnet-sdk-5.0.403-win-x64
+./configure
+make
+cd ../
+
+echo installing nodejs
+mkdir nodejs
+cd nodejs
+wget https://nodejs.org/download/release/v14.18.2/node-v14.18.2.tar.gz
+tar -xzf node-v14.18.2.tar.gz
+rm node-v14.18.2.tar.gz
+cd node-v14.18.2
+./configure
+make
+
+echo installing nodejs headers
+wget https://nodejs.org/download/release/v14.18.2/node-v14.18.2-headers.tar.gz
+tar -xzf node-v14.18.2-headers.tar.gz
+rm node-v14.18.2-headers.tar.gz
+cd node-v14.18.2-headers
+./configure
+make
 
 echo installing node-shared
 wget https://github.com/metacall/node.dll/releases/download/v0.0.1/node-shared-v14.18.2-x64.zip
 unzip node-shared-v14.18.2-x64.zip 
 rm node-shared-v14.18.2-x64.zip
+cd node-shared-v14.18.2-x64
+./configure
+make
 
 echo installing NodeJS DLL
-wget https://raw.githubusercontent.com/metacall/core/66fcaac300611d1c4210023e7b260296586a42e0/cmake/NodeJSGYPPatch.py  # --- check if this file is appropriate for macos
+wget https://raw.githubusercontent.com/metacall/core/66fcaac300611d1c4210023e7b260296586a42e0/cmake/NodeJSGYPPatch.py
 
 
 echo Building MetaCall
@@ -50,28 +96,28 @@ git clone --depth 1 https://github.com/metacall/core.git
 # Patch for FindNodeJS.cmake
 
 
-mkdir $loc/core/build
-cd $loc/core/build
+mkdir $LOC/core/build
+cd $LOC/core/build
 
 # Build MetaCall
 
-cmake -Wno-dev ^
-	-DCMAKE_BUILD_TYPE=Release ^
-	-DOPTION_BUILD_SECURITY=OFF ^
-	-DOPTION_FORK_SAFE=OFF ^
-	-DOPTION_BUILD_SCRIPTS=OFF ^
-	-DOPTION_BUILD_TESTS=OFF ^
-	-DOPTION_BUILD_EXAMPLES=OFF ^
-	-DOPTION_BUILD_LOADERS_PY=ON ^
-	-DOPTION_BUILD_LOADERS_NODE=ON ^
-	-DNPM_ROOT= $escaped_loc/runtimes/nodejs^  # change path
-	-DOPTION_BUILD_LOADERS_CS=ON ^
-	-DOPTION_BUILD_LOADERS_RB=ON ^
-	-DOPTION_BUILD_LOADERS_TS=ON ^
-	-DOPTION_BUILD_PORTS=ON ^
-	-DOPTION_BUILD_PORTS_PY=ON ^
-	-DOPTION_BUILD_PORTS_NODE=ON ^
-	-DCMAKE_INSTALL_PREFIX= $LOC ^    # change path
+cmake -Wno-dev \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DOPTION_BUILD_SECURITY=OFF \
+	-DOPTION_FORK_SAFE=OFF \
+	-DOPTION_BUILD_SCRIPTS=OFF \
+	-DOPTION_BUILD_TESTS=OFF \
+	-DOPTION_BUILD_EXAMPLES=OFF \
+	-DOPTION_BUILD_LOADERS_PY=ON \
+	-DOPTION_BUILD_LOADERS_NODE=ON \
+	-DNPM_ROOT= $escaped_loc/runtimes/nodejs\  # change path
+	-DOPTION_BUILD_LOADERS_CS=ON \
+	-DOPTION_BUILD_LOADERS_RB=ON \
+	-DOPTION_BUILD_LOADERS_TS=ON \
+	-DOPTION_BUILD_PORTS=ON \
+	-DOPTION_BUILD_PORTS_PY=ON \
+	-DOPTION_BUILD_PORTS_NODE=ON \
+	-DCMAKE_INSTALL_PREFIX= $LOC \    # change path
 	-G "NMake Makefiles" ..
 cmake --build . --target install
 
